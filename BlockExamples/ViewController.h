96091300a1b19ee0a8328f4e863c0645b9eceb53
Yury Lushkinou <yury_lushkinou@epam.com>
2019-03-28 16:41:03 +0300

    Initial Commit

---
+++ BlockExamples/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlockExamples
+//
+//  Created by Yury Lushkinov on 3/28/19.
+//  Copyright © 2019 Yury Lushkinov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BlockExamples/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  BlockExamples
+//
+//  Created by Yury Lushkinov on 3/28/19.
+//  Copyright © 2019 Yury Lushkinov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
